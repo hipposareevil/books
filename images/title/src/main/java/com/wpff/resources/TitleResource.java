10a5f1e80160dc30d4e034c632a4650d5e7b4956
hipposareevil <hipposareevil@gmail.com>
2017-07-20 10:13:36 -0500

    Updating dropwizard to 1.1.1 and new swagger

--- images/title/src/main/java/com/wpff/resources/TitleResource.java
+++ images/title/src/main/java/com/wpff/resources/TitleResource.java
@@ -23,10 +23,12 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
-import com.wordnik.swagger.annotations.ApiResponse;
+// Swagger
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ResponseHeader;
 
 
 /**
