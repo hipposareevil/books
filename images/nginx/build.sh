1225af8ba9e4d453b7b4d43afebab0a155f92a54
hipposareevil <hipposareevil@gmail.com>
2017-09-05 14:38:06 -0500

    Moving 'books.nginx' to 'books.gateway'

--- images/nginx/build.sh
+++ images/nginx/build.sh
@@ -3,7 +3,7 @@
 # Build nginx image
 
 # image we build
-imageName="books.frontend:latest"
+imageName="books.gateway:latest"
 
 # our real directory (so this can be called from outside directories)
 our_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
