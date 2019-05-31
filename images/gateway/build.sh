10988438af259e291c1c29ce83d91a7d4cbb6e7d
Samuel Jackson <samuel.jackson@salesforce.com>
2019-05-31 10:56:42 -0700

    Fixed gateway build

--- images/gateway/build.sh
+++ images/gateway/build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+############
 # Build nginx image
+# 
+############
 
-# image we build
-image_name="books.gateway:latest"
-
-# our real directory (so this can be called from outside directories)
+# Our real directory (so this can be called from outside directories)
 our_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Image we build
+project=$(cat $our_directory/webservice.name | xargs)
+project_version=$(cat $our_directory/../webservice.version | xargs)
+
+base_image_name="books.${project}"
+image_name="${base_image_name}:${project_version}"
 
 # set build time
 BUILD_TIME=$(date +%Y-%m-%dT%H:%M:%S%Z)
@@ -29,6 +36,17 @@ elapsed=$(expr $now - $then)
 if [ $build_result -eq 0 ]; then
     echo ""
     echo "[[Built $image_name in $elapsed second(s)]]"
+
+    # tag as latest
+    output=$(docker tag ${image_name} ${base_image_name}:latest)
+    tag_result=$?
+    if [ $tag_result -eq 0 ]; then
+        echo "[[Tagged \"${image_name}\" as \"${base_image_name}:latest\"]]"
+    else
+        echo "[[Unable to tag image as latest!!!!]]"
+    fi
+
+
 else
     echo ""
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
